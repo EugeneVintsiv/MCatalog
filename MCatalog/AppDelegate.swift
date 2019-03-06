aed1ab55be3733101e9fd9930196b5ae7a379c00
Eugene <eugene.vintsiv@gmail.com>
2019-03-06 21:57:23 +0200

    base project structure

---
+++ MCatalog/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  MCatalog
+//
+//  Created by Eugene on 3/6/19.
+//  Copyright © 2019 Eugene. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil)
+        -> Bool {
+            window = UIWindow(frame: UIScreen.main.bounds)
+            window?.makeKeyAndVisible()
+            
+            return true
+    }
+}
